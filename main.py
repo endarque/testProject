8462922f51edc0632282ba093ad2bd3fb900e70f
jjw1001@kosyas.com <rhsid2dlsgud>
2023-04-19 14:50:45 +0900

    //

---
+++ main.py
@@ -0,0 +1,22 @@
+import os
+from flask import Flask, render_template
+
+base_dir = os.path.dirname(os.path.abspath(os.path.dirname(__file__)))
+print(base_dir)
+project_dir = os.path.join(base_dir, 'testProject/')
+template_dir = os.path.join(project_dir, 'templates/html')
+print('temp' + template_dir)
+
+app = Flask(__name__, template_folder=template_dir)
+
+@app.route('/')
+def main():
+    return render_template('main.html')
+
+@app.route('/test')
+def test():
+    return render_template('main.html')
+
+if __name__ == "__main__":
+    print("server is running on localhost!!")
+    app.run(host='0.0.0.0', port=8888, debug=False)
